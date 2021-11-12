3cabe9929179a5cc3bd4e7a460373850aa19f811
Luan Silva <luan.owsla@gmail.com>
2021-11-12 17:20:47 -0300

    add prettier + eslint

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+
+import { routes } from './routes'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(routes)
+
+app.listen(process.env.PORT, () => {
+  console.log(`🚀 Server running on https://localhost:${process.env.PORT}`)
+})
